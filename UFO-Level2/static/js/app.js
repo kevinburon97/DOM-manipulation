ce09e0e97c80805a5e5e46fec2b50c042291ee40
unknown <kevinburon97@gmail.com>
2020-09-26 23:52:17 -0400

    finished bonus

--- UFO-Level2/static/js/app.js
+++ UFO-Level2/static/js/app.js
@@ -18,9 +18,10 @@ function populate(insertdata){
 populate(tableData)
 // Listen for search bar in order to show desired data
 
+// Filter for date
 
-var form = d3.select("#form1");
-form.on("submit",finddata);
+var form = d3.select("#datetime");
+form.on("input",finddata);
 
 
 
@@ -34,5 +35,56 @@ function finddata() {
     populate(filteredData)
     
 };
+// Filter for cities
+var form2 = d3.select("#city");
+form2.on("input",finddata2);
 
+function finddata2() {
+    d3.event.preventDefault();
+    
+    var input = d3.select("#city").property("value");
+  
+    var filteredData = tableData.filter(city => city.city === input);
+    
+    populate(filteredData)
+    
+};
+// Filter for State
+var form3 = d3.select("#state");
+form3.on("input",finddata3);
 
+function finddata3() {
+    d3.event.preventDefault();
+    
+    var input = d3.select("#state").property("value");
+  
+    var filteredData = tableData.filter(state => state.state === input);
+    
+    populate(filteredData)
+};
+// Filter for Country
+var form4 = d3.select("#country");
+form4.on("input",finddata4);
+
+function finddata4() {
+    d3.event.preventDefault();
+    
+    var input = d3.select("#country").property("value");
+  
+    var filteredData = tableData.filter(country => country.country === input);
+    
+    populate(filteredData)
+};
+// Filter for Shape
+var form5 = d3.select("#shape");
+form5.on("input",finddata5);
+
+function finddata5() {
+    d3.event.preventDefault();
+    
+    var input = d3.select("#shape").property("value");
+  
+    var filteredData = tableData.filter(shape => shape.shape === input);
+    
+    populate(filteredData)
+};
